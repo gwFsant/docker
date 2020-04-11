adab54545cb05e028d237e4cd3aa7410a37e880c
Felipe Roldan <f.roldan@greenworkconsultores.com>
2020-04-11 13:21:31 -0500

    Asignacion de volumen en combinacion con docker-compose

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     if (err) {
       res.status(500).send('💥 Booa 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me reconecté a la DB! 😎');
       db.close();
     }
   });
